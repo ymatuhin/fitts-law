72c9f5a91ceb1d2bbbc64eba1acbc8bf51675d9a
ymatuhin <ymatuhin@poloniumarts.com>
2014-06-02 16:09:47 +0400

    add| first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+app = angular.module('app', ['ngRoute']);
+
+app.config([
+  '$routeProvider', function($routeProvider, $routeParams) {
+    $routeProvider.when('/:id', {
+        templateUrl: function(params) {
+          if (params.id > 30) {
+            params.id = 1;
+            location.hash = '#/1'
+          }
+
+          return 'html/' + params.id + '.html';
+        },
+        controller: 'indexCtrl'
+    }, { id: "[0-9]" })
+    .otherwise({
+      templateUrl: 'html/1.html',
+      controller: 'indexCtrl'
+    });
+  }
+]);
+
+app.run(function($rootScope) {
+  $rootScope.hasDevice = true
+});
